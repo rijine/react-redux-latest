38bf0e784bf5cbac23842e583cc4be9fd76ef0dc
rijine <e.rijin@gmail.com>
2018-05-04 00:50:51 +0530

    Links for routing

--- src/ExpenseApp.js
+++ src/ExpenseApp.js
@@ -1,17 +1,37 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 
 export const ExpenseDashboardPage = () => <div>Dashboard Page</div>;
 export const AddExpensePage = () => <div>Add Expense Page</div>;
-export const PageNotFound = () => <div>404 Page Not Found</div>;
+export const HelpPage = () => <div>Helps Page</div>;
+export const PageNotFound = () => (
+  <div>
+    404 Page Not Found
+    <Link to="/">Go Home</Link>
+  </div>
+);
+
+export const Header = () => (
+  <header>
+    <h1>Expensify</h1>
+    <Link to="/">Dashboard</Link>
+    <Link to="/create">Create</Link>
+    <Link to="/edit">Edit</Link>
+    <Link to="/help">Help</Link>
+  </header>
+);
 
 const routes = (
   <BrowserRouter>
-    <Switch>
-      <Route exact={true} path="/" component={ExpenseDashboardPage} ></Route>
-      <Route path="/create" component={AddExpensePage} />
-      <Route component={PageNotFound} />
-    </Switch>
+    <div>
+      <Header />
+      <Switch>
+        <Route exact={true} path="/" component={ExpenseDashboardPage} />
+        <Route path="/create" component={AddExpensePage} />
+        <Route path="/help" component={HelpPage} />
+        <Route component={PageNotFound} />
+      </Switch>
+    </div>
   </BrowserRouter>
 );
 
